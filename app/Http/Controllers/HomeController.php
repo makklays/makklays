9432d92d718fd79c556df4136502279e5589e2bd
Olexander Kuziv (OK) <makklays@gmail.com>
2019-10-06 22:55:44 +0300

    add Trait :)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Traits\MyTraits;
 
 class HomeController extends Controller
 {
+    use MyTraits;
+
     /**
      * Create a new controller instance.
      *
@@ -23,6 +26,8 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // echo $this->hello(); // example uses trait
+
         return view('home');
     }
 }
